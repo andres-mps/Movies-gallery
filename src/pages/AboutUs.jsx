d7fc278b894e6ec3f4ccb37526c1f14c0eef863e
andres-mps <andresmps@gmail.com>
2023-07-18 07:46:38 -0300

    subida proyecto

--- src/pages/AboutUs.jsx
+++
@@ -1,9 +0,0 @@
-function AboutUs() {
-  return (
-    <div className="container text-white " style={{ height: "100vh" }}>
-      <h1>Te damos la bienvenida a SOBRE NOSOTROS</h1>
-    </div>
-  );
-}
-
-export default AboutUs;
--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact() {
-  return (
-    <div className="container text-white" style={{ height: "100vh" }}>
-      <h1>Te damos la bienvenida a CONTACTO</h1>
-    </div>
-  );
-}
-
-export default Contact;
