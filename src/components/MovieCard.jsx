bf542f4eb614de97101a1b6ab7eefef034dc98d4
andres-mps <andresmps@gmail.com>
2023-07-31 10:42:56 -0300

    modal restablecido

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
 import "./MovieCard.css";
-// import MovieModal from "./MovieModal";
-import { Link, NavLink } from "react-router-dom";
+import MovieModal from "./MovieModal";
+import { useState } from "react";
 
 function MovieCard({ movie }) {
   const ratingStars = {
@@ -11,19 +11,22 @@ function MovieCard({ movie }) {
     isHalf: true,
     edit: false,
   };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className="col-6 col-md-4 col-lg-3">
-      <Link to={`/pelicula/${movie.id}`}>
-        <div className="card d-flex flex-column align-items-center">
-          <img
-            className="fluid w-100 h-100 my-3"
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={movie && movie.original_title}
-          />
-          <ReactStars {...ratingStars} />
-        </div>
-      </Link>
+      <div className="card d-flex flex-column align-items-center" onClick={() => setShow(true)}>
+        <img
+          className="fluid w-100 h-100 my-3"
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie && movie.original_title}
+        />
+        <ReactStars {...ratingStars} />
+      </div>
+      <MovieModal movie={movie} handleClose={handleClose} show={show} />
     </div>
   );
 }
